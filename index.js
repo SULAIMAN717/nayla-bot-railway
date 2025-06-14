d96b21d11ab760d533cb4319aead5b0908266016
SULAIMAN717 <wkccianjur6@gmail.com>
2025-06-15 02:18:43 +0700

    Add files via upload
    upload file bot nayla GPT ke github

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const client = new Client();
+client.on('qr', (qr) => { qrcode.generate(qr, { small: true }); });
+client.on('ready', () => { console.log('Nayla Bot aktif!'); });
+client.initialize();
